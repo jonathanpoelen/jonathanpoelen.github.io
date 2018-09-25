ee4f3a3a7b04922ef3cd6be3c8efec4f8e134829
Jonathan Poelen <jonathan.poelen@gmail.com>
2018-09-26 00:26:41 +0200

    update.sh: post-modification to replace the ``` output to chroma class

--- update.sh
+++ update.sh
@@ -6,6 +6,10 @@ cd "$(dirname "$0")"
 
 ./generate.sh
 
+# fix ```
+find public \( -path 'public/2???/*' -or -path 'public/page/*' \) -type f -name '*.html' -exec \
+  sed -i 's#<pre><code>#<div class="highlight"><pre class="chroma"><code>#;s#</code></pre>$#</code></pre></div>#' '{}' +
+
 cd public
 git add .
 git commit -m "Site updated at $(LANG=en date --rfc-3339=seconds -u)"
@@ -14,4 +18,4 @@ git push
 [ -z "$@" ] && wget \
   'www.google.com/webmasters/tools/ping?sitemap=http://jonathanpoelen.github.io/sitemap.xml' \
   'www.bing.com/webmaster/ping.aspx?siteMap=http://jonathanpoelen.github.io/sitemap.xml' \
-  -O /dev/null
+  -O /dev/null || :
