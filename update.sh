e3a29f065197c95cc3b438eb164a42275eb7627f
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-10-29 00:37:21 +0200

    update.sh: add new tabindex"0"

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 
 # fix ```
 find public \( -path 'public/2???/*' -or -path 'public/page/*' \) -type f -name '*.html' -exec \
-  sed -i '/<pre><code>/,/<\/code><\/pre>/{s#<pre><code>#<div class="highlight-nocode"><pre class="chroma-nocode"><code>#;N;s#^</code></pre>#</code></pre></div>#}' '{}' +
+  sed -i '/<pre tabindex="0"><code>/,/<\/code><\/pre>/{s#<pre tabindex="0"><code>#<div class="highlight-nocode"><pre tabindex="0" class="chroma-nocode"><code>#;N;s#^</code></pre>#</code></pre></div>#}' '{}' +
 
 cd public
 git add .
